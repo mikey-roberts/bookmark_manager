0706620f5eadc8ac2050452ca9abd24c40c80361
mikey-roberts <mikey_roberts@outlook.com>
2021-06-15 12:13:22 +0100

    tests created for bookmark logic

--- spec/viewing_bookmarks_spec.rb
+++ spec/viewing_bookmarks_spec.rb
@@ -3,4 +3,14 @@ feature 'Viewing bookmarks' do
     visit('/')
     expect(page).to have_content "Bookmark Manager"
   end
+
+  feature 'Viewing bookmarks' do
+    scenario 'A user can see bookmarks' do
+      visit('/bookmarks')
+  
+      expect(page).to have_content "http://www.makersacademy.com"
+      expect(page).to have_content "http://www.destroyallsoftware.com"
+      expect(page).to have_content "http://www.google.com"
+    end
+  end
 end
